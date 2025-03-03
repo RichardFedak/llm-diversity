5f48f9fae01a5794f55df0608c9c88b7511cdaa6
RichardF <richardfedak@gmail.com>
2025-03-03 10:56:08 +0100

    Update metric orderings

--- data/final_data/magnitude_assessment/extract_movie_metrics.py
+++ data/final_data/magnitude_assessment/extract_movie_metrics.py
@@ -1,33 +1,24 @@
 import json
 
 input_file = 'final_movie_data.json'
-output_file = 'metrics.json'
 
 fields_to_extract = ["cf_ild", "cb_ild", "ease_ild", "genres", "tags", "bin_div"]
 
-
-with open(input_file, 'r') as infile:
-    data = json.load(infile)
-
 def process_field(value):
-    values_list = json.loads(value)  # Convert string to list
-    if len(values_list) == 3 and len(set(values_list)) < 3:
-        return []  # Return empty list if any two values are the same
-    return [i[0] for i in sorted(enumerate(values_list), key=lambda x: x[1])]
+    values = json.loads(value)
+    unique_values = set(values)
+    if len(values) == 3 and len(unique_values) < 3:
+        return ["X" if values.count(v) > 1 else i for i, v in enumerate(values)]
+    return [i[0] for i in sorted(enumerate(values), key=lambda x: x[1])]
 
-extracted_data = []
-for item in data:
-    filtered_item = {}
-    alphas = [float(x.strip()) for x in item['alphas'][1:-1].split(',')]
-    if set([0.0, 0.01]).issubset(alphas) or set([1.0, 0.99]).issubset(alphas):
-        continue
-    for key in fields_to_extract:
-        if key in item:
-            filtered_item[key] = process_field(item.get(key))
-    extracted_data.append(filtered_item)
-
-with open(output_file, 'w') as outfile:
-    json.dump(extracted_data, outfile, indent=4)
-
-print(f"Filtered data saved successfully to '{output_file}'")
-
+with open(input_file, 'r+') as file:
+    data = json.load(file)
+    
+    for item in data:
+        for key in fields_to_extract:
+            if key in item:
+                item[key] = process_field(item[key])
+    
+    file.seek(0)
+    json.dump(data, file, indent=4)
+    file.truncate()
