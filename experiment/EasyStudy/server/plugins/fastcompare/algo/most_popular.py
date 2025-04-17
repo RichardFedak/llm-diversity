b20772f74d54f5781e400c684fe4705b2121065f
RichardF <richardfedak@gmail.com>
2025-04-17 14:47:03 +0200

    add algo

---
+++ experiment/EasyStudy/server/plugins/fastcompare/algo/most_popular.py
@@ -0,0 +1,36 @@
+from abc import ABC
+
+from plugins.fastcompare.algo.algorithm_base import (
+    AlgorithmBase,
+)
+
+
+class MostPopular(AlgorithmBase, ABC):
+
+    def __init__(self, loader, **kwargs):
+        self._ratings_df = loader.ratings_df
+        self._loader = loader
+        self._all_items = self._ratings_df.item.unique()
+
+    def fit(self):
+        pass
+
+    # Predict for the user
+    def predict(self, selected_items, filter_out_items, k):
+        ratings = self._ratings_df.groupby("item").size().sort_values(ascending=False)
+
+        result = ratings.index.values.tolist()
+
+        result = [r for r in result if r not in filter_out_items][:k]
+
+        return result
+
+    @classmethod
+    def name(cls):
+        return "MostPopular"
+
+    @classmethod
+    def parameters(cls):
+        return [
+            
+        ]
